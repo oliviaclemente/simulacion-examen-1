ae4987a14ba0e8532eeccecf1a7337c38849c3fb
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-17 12:06:58 +0000

    cc

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ def countApplesAndOranges(s, t, apple, orange, apples, oranges):
     temp= orange + oranges[i]
     if(temp in range(s,t+1)):
       orangecount += 1
+  print(applecount)
+  print(orangecount)
+
+if __name__ == '__main__':
+  first_multiple_input = input().rstrip().split()
+  s = int(first_multiple_input[0])
+  t = int(first_multiple_input[1])
+  second_multiple_input = input().rstrip().split()
+  apple = int(second_multiple_input[0])
+  orange = int(second_multiple_input[1])
+  third_multiple_input = input().rstrip().split()
+  m = int(third_multiple_input[0])
+  n = int(third_multiple_input[1]
+  
