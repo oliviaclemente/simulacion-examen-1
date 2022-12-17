d07e4f81d0dd35616c3273b7481d43a965c1c766
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-17 13:29:52 +0000

    cc

---
+++ ejercicios/ej4.py
@@ -0,0 +1,25 @@
+def staircase(n):
+  for i in range(1, n+1):
+    print(" "*(n-i)+"#"*(i))
+  def staircase(n):
+    for row in range(n):
+      string=""
+      for col in range(n):
+        if col <= row:
+          string += "#"
+        else: 
+          string= " " + string
+      print(string)
+      
+    def staircase(n, row=0, string=""):
+      if n == row:
+        return
+      if n== len(string):
+        print(string)
+        return staircase(n, row+1)
+    add = "#" if len(string) <= row else" "
+    staircase(n, row, add+ string)  
+  
+if __name__ == '__main__':
+  n = int(input().strip())
+  staircase(n)
