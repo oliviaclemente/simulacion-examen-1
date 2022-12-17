aa9de64d88031276d2cb297599190fc95f3ba2d3
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-17 13:36:55 +0000

    c

--- ejercicios/ej1.py
+++ ejercicios/ej1.py
@@ -1,6 +1,15 @@
-n= int(input("Dame una matrix 3x3:"))
-num= list(map(int, input().split))
-sum=0
-for n1 in num:
-  sum += n1
-print(sum)
+def simpleArraySum(ar):
+  n= int(input("Dame una matrix 3x3:"))
+  num= list(map(int, input().split))
+  sum=0
+  for n1 in num:
+    sum += n1
+  print(sum)
+
+if __name__ == '__main__':
+  fptr = open(os.environ['OUTPUT_PATH'], 'w')
+  ar_count = int(input().strip())
+  ar = list(map(int, input().rstrip().split()))
+  result = simpleArraySum(ar)
+  fptr.write(str(result) + '\n')
+  fptr.close()
--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-def compareTriplets(a, b):  
-  a1= 0
-  b1= 0
-  for i in range(0,3):
-    if a[i]>b[i]:
-      a1 += 1
-    if a[i]<b[i]:
-      b1 += 1
-    return[a1,b1]
-  
+def aVeryBigSum(ar):
+  sum= 0
+  for i in renge(len(ar)):
+    sum +=ar[i]
+  return sum
+
+
 if __name__ == '__main__':
   fptr = open(os.environ['OUTPUT_PATH'], 'w')
-  a = list(map(int, input().rstrip().split()))
-  b = list(map(int, input().rstrip().split()))
-  result = compareTriplets(a, b)
-  fptr.write(' '.join(map(str, result)))
-  fptr.write('\n')
+  ar_count = int(input().strip())
+  ar = list(map(int, input().rstrip().split()))
+  result = aVeryBigSum(ar)
+  fptr.write(str(result) + '\n')
   fptr.close()
