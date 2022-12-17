b39e18413c129754ff7fd5d15f822143858c97a9
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-17 13:20:29 +0000

    c

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ def staircase(n):
     string=""
     for col in range(n):
       if col <= row:
-        string
+        string += "#"
+      else: 
+        string= " " + string
+    print(string)
+    
+def staircase(n, row=0, string=""):
+  if n == row:
+    return
+  if n== len(string):
+    print(string)
+    return staircase(n, row+1)
+  add = "#" if len(string) <= row else" "
+  staircase(n, row, add+ string)        
       
   
 
